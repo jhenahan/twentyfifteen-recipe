810948b2c8a75448eca6c97abd04971816101755
Jack Henahan <jhenahan@me.com>
2015-04-22 05:30:09 -0400

    Initial commit

---
+++ functions.php
@@ -0,0 +1,34 @@
+<?php
+
+add_action( 'wp_enqueue_scripts', 'twentyfifteen_parent_theme_enqueue_styles' );
+
+function twentyfifteen_parent_theme_enqueue_styles() {
+    wp_enqueue_style( 'twentyfifteen-style', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( '-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array('twentyfifteen-style')
+    );
+
+}
+
+function multibox_microdata_list_render($arr, $item_prop = '') {
+    if (empty($item_prop)) {
+        $list_item = '<li>';
+    }
+    else {
+        $list_item = '<li itemprop="' . $item_prop . '">"';
+    }
+    foreach ( $arr as $key => $value ) {
+        $serialized = maybe_unserialize( $value );
+        foreach ( $serialized as $key ) {
+            $output = $list_item;
+            foreach ( $key as $field => $value ) {
+                $output .= $value;
+                $output .= ' ';
+            }
+            $output .= '</li>';
+            echo $output;
+        }
+
+    }
+}
---
+++ single-recipe.php
@@ -0,0 +1,98 @@
+<?php
+
+/**
+ * The template for displaying all single recipes
+ *
+ * @package    WordPress
+ * @subpackage Twenty_Fifteen_Recipes
+ * @since      Twenty Fifteen Recipes 1.0
+ */
+
+get_header(); ?>
+
+<div id="primary" class="content-area">
+    <main id="main" class="site-main" role="main">
+
+        <?php
+        $args = array(
+            'post_type' => 'recipe',
+            'posts_per_page' => 10
+        );
+        $loop = new WP_Query( $args );
+        // Start the loop.
+        while ($loop->have_posts()) :
+        $loop->the_post();
+        $metas = get_post_meta( get_the_ID() );
+        extract( $metas );
+        ?>
+        <div itemscope itemtype="http://schema.org/Recipe">
+            <header class="entry-header">
+                <?php the_title(
+                    '<h1 class="entry-title" itemprop="name">', '</h1>'
+                ); ?>
+                <p>By <span itemprop="author"><?php the_author(); ?></span></p>
+            </header>
+            <div class="entry-content">
+                <div itemprop="description">
+                    <?php the_content(); ?>
+                </div>
+                <div><span
+                        itemprop="recipeYield"><?php echo $recipe_yield[ 0 ]?></span>
+                    Serving
+                </div>
+                <div>
+                    Prep Time:
+                    <time
+                        itemprop="prepTime"><?php echo $recipe_prep_time[ 0 ] ?></time>
+                    <br/>
+                    Cook Time:
+                    <time
+                        itemprop="cookTime"><?php echo $recipe_cook_time[ 0 ] ?></time>
+                    <br/>
+                    Ready In:
+                    <time
+                        itemprop="totalTime"><?php echo $recipe_total_time[ 0 ] ?></time>
+                    <br/>
+                </div>
+                <div>
+                    <h3> Ingredients</h3>
+                    <ul>
+                        <?php
+                        multibox_microdata_list_render(
+                            $recipe_ingredients, 'ingredients'
+                        );
+                        ?>
+                    </ul>
+                </div>
+
+                <div itemprop="recipeInstructions">
+                    <h3> Directions</h3>
+                    <ol>
+                        <?php
+                        multibox_microdata_list_render( $recipe_instructions );
+                        ?>
+                    </ol>
+                </div>
+
+                <div itemprop="nutrition">
+                    <span itemscope
+                          itemtype="http://schema.org/NutritionInformation">
+                        <h3> Nutritional Information </h3>
+                        <p><strong>Amount Per Serving</strong>&nbsp; Calories:
+                            <span itemprop="calories">405</span>
+                            | Total Fat: <span itemprop="fatContent">18g</span>
+                            |
+                            Cholesterol: <span itemprop="cholesterolContent">44mg</span>
+                        </p>
+      </span>
+                </div>
+            </div>
+            <?php
+            endwhile;
+            ?>
+
+    </main>
+    <!-- .site-main -->
+</div><!-- .content-area -->
+
+<?php get_footer(); ?>
